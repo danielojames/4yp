3eb9ff90410e01927fd2a0a6fa4ed19b72d54eb7
Dan <daniel.o.james@gmail.com>
2014-11-05 12:54:45 +0000

    Mean calculation corrected

--- ShiftingMethod/get_average_shape.m
+++ ShiftingMethod/get_average_shape.m
@@ -7,22 +7,24 @@ function shape = get_average_shape(pixels,light_level)
         ref_indexes(i) = find(light_level == ref_levels(i),1);
     end
     
-    ref_pixel = pixels(:,1);
-    
-    shifts(1) = 0;
-    
+    % choose and plot reference pixel
+    ref_pixel = pixels(:,1);    
+    shifts(1) = 0;    
     figure;
-    hold on;
-    
+    hold on;    
     plot(ref_pixel,light_level)
     
     for i=2:size(pixels,2)
         shift = 0;
         for j=1:length(ref_indexes)
+            % find difference for each light level
             shift = shift + pixels(ref_indexes(j),i) - ref_pixel(ref_indexes(j));
         end
-        shifts(i) = shift / (length(ref_indexes) - 1);
         
+        % take mean of shifts as the shift value 
+        shifts(i) = shift / length(ref_indexes);
+        
+        % plot shifted data for each pixel
         plot(pixels(:,i) - shifts(i),light_level)
     end
     
